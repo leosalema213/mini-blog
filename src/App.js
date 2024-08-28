6bf06441fdbbfc8bd5d9fe3faa4fa99b31759ca5
Leonardo Salema Dev <leo_viciogta@hotmail.com>
2024-08-28 14:16:54 -0300

    chore: importando tudo no app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -10,19 +14,40 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 
 import "./App.css";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const {auth} = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(()=> {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  },[auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="app">
-      <AuthProvider>
+      <AuthProvider value={{user}}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/lOgin" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={ !user ? <Login />  : <Navigate to="/" />} />
+              <Route path="/register" element={ !user ? <Register />  : <Navigate to="/" />} />
+              <Route path="/posts/create" element={user ? <CreatePost />  : <Navigate to="/login" />} />
+              <Route path="/dashboard" element={user ? <Dashboard />  : <Navigate to="/login" />} />
             </Routes>
           </div>
           <Footer />
