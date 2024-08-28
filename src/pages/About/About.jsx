69ec7c9f1952c22a65426d469b1d13b3ed1de3bf
Leonardo Salema Dev <leo_viciogta@hotmail.com>
2024-08-28 14:16:26 -0300

    feat: colocando conteudo na pagina de about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,19 @@
-import styles from "./about.module.css"
+import { Link } from "react-router-dom";
+import styles from "./about.module.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Esse projeto consiste em um blog feito com React no front-end e Firebase
+        no back-end.
+      </p>
+      <Link to="/posts/create" className="btn">Criar Post</Link>
+    </div>
+  );
+};
 
-export default About
+export default About;
