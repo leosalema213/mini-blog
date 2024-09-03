8f0b689c21ad7a3c4272d994cce28e049ee727fe
Leonardo Salema Dev <leo_viciogta@hotmail.com>
2024-09-03 12:49:01 -0300

    chore: criando  e  importando componente de edicao

--- src/App.js
+++ src/App.js
@@ -18,28 +18,27 @@ import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
 import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 
 function App() {
   const [user, setUser] = useState(undefined);
-  const {auth} = useAuthentication();
+  const { auth } = useAuthentication();
 
   const loadingUser = user === undefined;
 
-  useEffect(()=> {
-
+  useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setUser(user)
-    })
+      setUser(user);
+    });
+  }, [auth]);
 
-  },[auth])
-
-  if(loadingUser) {
-    return <p>Carregando...</p>
+  if (loadingUser) {
+    return <p>Carregando...</p>;
   }
 
   return (
     <div className="app">
-      <AuthProvider value={{user}}>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
@@ -48,10 +47,26 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/search" element={<Search />} />
               <Route path="/posts/:id" element={<Post />} />
-              <Route path="/login" element={ !user ? <Login />  : <Navigate to="/" />} />
-              <Route path="/register" element={ !user ? <Register />  : <Navigate to="/" />} />
-              <Route path="/posts/create" element={user ? <CreatePost />  : <Navigate to="/login" />} />
-              <Route path="/dashboard" element={user ? <Dashboard />  : <Navigate to="/login" />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Login />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
             </Routes>
           </div>
           <Footer />
